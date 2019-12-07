46234a1c64a4103e0d1efedcb6be507622d4d03f
springdoc <springdoc99@gmail.com>
2019-12-07 21:16:27 +0100

    project update

--- ddd-sample-exposition/src/main/java/fr/training/spring/shop/SpringBootApp.java
+++ ddd-sample-exposition/src/main/java/fr/training/spring/shop/SpringBootApp.java
@@ -22,6 +22,6 @@ public class SpringBootApp {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringBootApp.class, args);
-        LOGGER.info("Started!");
+        LOGGER.info("Started Again!");
     }
 }
